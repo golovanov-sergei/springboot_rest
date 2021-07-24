2fcb64af1c8b0974e60864ef2c1f62a7d8b023a7
imp <imp.sergei@gmail.com>
2021-07-24 19:14:57 +0300

    99. Использование JPA в REST API проекте

--- src/main/java/com/tregulov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/tregulov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -1,12 +1,13 @@
 package com.tregulov.spring.springboot.springboot_rest.dao;
 
 import com.tregulov.spring.springboot.springboot_rest.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -16,38 +17,43 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-
-        //        List<Employee> allEmployees = session.createQuery("from Employee ",Employee.class).getResultList();
-
-        Query<Employee> query = session.createQuery("from Employee ", Employee.class);
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        //        List<Employee> allEmployees = session.createQuery("from Employee ",Employee.class).getResultList();
+//
+//        Query<Employee> query = session.createQuery("from Employee ", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
+        Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
-
-
         return allEmployees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-//        if (employee.getId() == 0)
-//            session.save(employee);
-//        else session.update(employee);
-        session.saveOrUpdate(employee);
+//        Session session = entityManager.unwrap(Session.class);
+////        if (employee.getId() == 0)
+////            session.save(employee);
+////        else session.update(employee);
+//        session.saveOrUpdate(employee);
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = session.get(Employee.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//        Employee employee = session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
+//        Session session = entityManager.unwrap(Session.class);
 //        session.delete(session.get(Employee.class,id));
-        Query<Employee> query = session.createQuery("delete from Employee " +
+//        Query<Employee> query = session.createQuery("delete from Employee " +
+//                "where id=:employeeId");
+        Query query = entityManager.createQuery("delete from Employee " +
                 "where id=:employeeId");
         query.setParameter("employeeId",id);
         query.executeUpdate();
